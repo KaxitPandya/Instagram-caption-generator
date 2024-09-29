bc1d972a3e2abb56549227502d72389a5acd4773
KaxitPandya <kaxitpandya@gmail.com>
2024-09-29 00:38:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # add your Google API key in the secrets.toml file as:
-GOOGLE_API_KEY = 'your_api_key'
+# GOOGLE_API_KEY = 'your_api_key'
 # this key will be used when deploying your Streamlit app.
 api_key = st.secrets["GOOGLE_API_KEY"]
 lang = st.selectbox("Select Language:", ['english(default)', 'French'], index=None)
